04adf501ca0ec6c5b41df6e9e31dddbea54a40c5
Djax Bank <djax@djax.dev>
2025-06-25 16:07:56 +0200

    Add parser changes with known segfault

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dbank <dbank@student.codam.nl>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 12:22:29 by dbank             #+#    #+#             */
-/*   Updated: 2025/06/25 14:30:44 by dbank            ###   ########.fr       */
+/*   Updated: 2025/06/25 16:01:10 by dbank            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 
 int	main(int argc, char *argv[])
 {
-	t_state state;
+	t_state		state;
 	s_mlx		box;
-	// s_game_data game;
 	
 	ft_bzero(&state, sizeof(t_state));
 	if (argc != 2 || input_check(argv[1], &state) != 1)
@@ -28,13 +27,11 @@ int	main(int argc, char *argv[])
 	}
 	// map_init(&state);
 
-	box.background = mlx_load_png("textures/Background.png");
 	box.block = mlx_load_png("textures/block.png");
 	box.mlx = mlx_init(800, 800, "Cub3d", false);
-	box.background_image = mlx_texture_to_image(box.mlx, box.background);
-	mlx_resize_image(box.background_image, 800, 800);
-	mlx_image_to_window(box.mlx, box.background_image, 0, 0);
 	mlx_key_hook(box.mlx, key_hook, &box);
+	mlx_loop_hook(box.mlx, loop_hook, &box);
+	render_background(12, 127, &box);
 	mlx_loop(box.mlx);
 	mlx_terminate(box.mlx);
 	return 0;
--- src/rendering.c
+++ src/rendering.c
@@ -6,21 +6,35 @@
 /*   By: dbank <dbank@student.codam.nl>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 14:24:43 by dbank             #+#    #+#             */
-/*   Updated: 2025/06/25 14:30:29 by dbank            ###   ########.fr       */
+/*   Updated: 2025/06/25 16:02:01 by dbank            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 #include "parse.h"
+#define y 0
+#define x 1
+#define BPP sizeof(int32_t)
 
-void key_hook(struct mlx_key_data key, void *param)
+void	loop_hook(void *param)
 {
-	static int count;
-	static int size = 100;
-	mlx_image_t *image;
+	#include <stdio.h>
+	static unsigned int c;
 	s_mlx *box;
-	box = param;
+
+	box = param; 
+	printf("%d\n", c++);
+}
+
+void	key_hook(struct mlx_key_data key, void *param)
+{
+	static int	count;
+	static int	size = 100;
+	mlx_image_t	*image;
+	s_mlx		*box;
+
 	(void)key;
+	box = param;
 	if (mlx_is_key_down(box->mlx, MLX_KEY_ESCAPE))
 		return (mlx_close_window(box->mlx));
 	else if (!mlx_is_key_down(box->mlx, MLX_KEY_RIGHT))
@@ -30,4 +44,27 @@ void key_hook(struct mlx_key_data key, void *param)
 	mlx_image_to_window(box->mlx, image, count * 80, 0 + (300 + (count * 2)));
 	count++;
 	size -= 10;
+}
+
+void render_background(const int ceilingc, const int floorc, s_mlx *mlx)
+{
+
+	mlx->ceiling = mlx_new_image(mlx->mlx, 800, 400);
+	mlx->floor = mlx_new_image(mlx->mlx, 800, 400);
+	ft_memset(mlx->ceiling, ceilingc, 800 * 400 * BPP);
+	ft_memset(mlx->floor, floorc, 800 * 400 * BPP);
+	mlx_image_to_window(mlx->mlx, mlx->ceiling, 0, 0);
+	mlx_image_to_window(mlx->mlx, mlx->floor, 0, 400);
+}
+
+void	render_window(s_game_data *data)
+{
+	int pos[2];
+	size_t i;
+
+	i = 0;
+	pos[x] = data->player.pos[x];
+	pos[y] = data->player.pos[y];
+	while (data->map[y][x] == '0' || data->map[y][x] == 'P');
+	
 }
