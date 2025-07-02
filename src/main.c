06ff31f13a3a5b301f69ea11c56e27353ff6a8c4
Djax Bank <djax@djax.dev>
2025-07-02 23:54:55 +0200

    first raycaster version

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dbank <dbank@student.codam.nl>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 12:22:29 by dbank             #+#    #+#             */
-/*   Updated: 2025/06/30 16:26:18 by dbank            ###   ########.fr       */
+/*   Updated: 2025/07/02 23:44:03 by dbank            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,13 @@ int	main(int argc, char *argv[])
 	map_init(&data);
 	data.mlx.block = mlx_load_png("textures/block.png");
 	data.mlx.mlx = mlx_init(800, 800, "Cub3d", false);
-	mlx_key_hook(data.mlx.mlx, key_hook, &data.mlx.mlx);
-	// mlx_loop_hook(data.mlx.mlx, raycaster, &data);
+	data.mlx.wall = malloc(800 * sizeof(mlx_image_t *));
+	data.player.orientation = -M_PI/2;
+	mlx_key_hook(data.mlx.mlx, key_hook, &data);
 	render_background((int[3]){40, 40, 120}, (int[3]){50, 110, 40}, &data.mlx);
+	raycaster(&data);
 	mlx_loop(data.mlx.mlx);
 	mlx_terminate(data.mlx.mlx);
+	free(data.mlx.wall);
 	return 0;
 }
