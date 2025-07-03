0b8879d5f80d73b1e18729c0812cb4caf01008c4
a0f <samspencerhoward@gmail.com>
2025-07-03 16:51:52 +0200

    updated player movement to be consistent. added collision check

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dbank <dbank@student.codam.nl>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/25 12:22:29 by dbank             #+#    #+#             */
-/*   Updated: 2025/07/03 16:16:35 by dbank            ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dbank <dbank@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/06/25 12:22:29 by dbank         #+#    #+#                 */
+/*   Updated: 2025/07/03 16:51:07 by showard       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,41 @@ void	init_textures(t_data *data, t_mlx *mlx)
 		werror("Failure loading EA texture.", data);
 }
 
+static void collision_check(t_data *game, float new_y, float new_x)
+{
+	if (game->map[(int)(new_y - 0.1)][(int)(new_x - 0.1)] != '1'
+		&& game->map[(int)(new_y - 0.1)][(int)(new_x + 0.1)] != '1' 
+		&& game->map[(int)(new_y + 0.1)][(int)(new_x - 0.1)] != '1' 
+		&& game->map[(int)(new_y + 0.1)][(int)(new_x + 0.1)] != '1')
+	{
+		game->player.pos_x = new_x;
+        game->player.pos_y = new_y;
+	}
+}
+
+void loop_hook(void *param)
+{
+    t_data *game = (t_data *)param;
+
+    if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_W))
+    {
+        float new_y = game->player.pos_y + sin(game->player.orientation) / 20;
+    	float new_x = game->player.pos_x + cos(game->player.orientation) / 20;
+       	collision_check(game, new_y, new_x);
+    }
+    if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_S))
+    {
+        float new_y = game->player.pos_y - sin(game->player.orientation) / 20;
+        float new_x = game->player.pos_x - cos(game->player.orientation) / 20;
+		collision_check(game, new_y, new_x);
+	}
+	if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_A))
+		game->player.orientation -= 0.05;
+	if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_D))
+		game->player.orientation += 0.05;
+	raycaster(game);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_data		data;
@@ -56,7 +91,9 @@ int	main(int argc, char *argv[])
 	data.mlx.block = mlx_load_png("textures/block.png");
 	data.mlx.mlx = mlx_init(800, 800, "Cub3d", false);
 	data.player.orientation = set_orientation(data.map[(int)data.player.pos_y][(int)data.player.pos_x]);
-	mlx_key_hook(data.mlx.mlx, key_hook, &data);
+	// i didn't rip out all of the key hook functions yet.
+	// mlx_key_hook(data.mlx.mlx, key_hook, &data);
+	mlx_loop_hook(data.mlx.mlx, loop_hook, &data);
 	render_background(data.ceiling, data.floor, &data.mlx);
 	raycaster(&data);
 	mlx_loop(data.mlx.mlx);
