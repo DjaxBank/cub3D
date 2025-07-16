668d26926921e307527abcdaf7d45842391510dd
Djax Bank <djax@djax.dev>
2025-07-16 15:17:50 +0200

    made doors

--- src/movement.c
+++ src/movement.c
@@ -6,62 +6,60 @@
 /*   By: dbank <dbank@student.codam.nl>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 18:08:10 by showard           #+#    #+#             */
-/*   Updated: 2025/07/15 18:42:22 by dbank            ###   ########.fr       */
+/*   Updated: 2025/07/16 14:07:29 by dbank            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 
-void try_x_movement(t_data *game, float new_x)
+void	try_x_movement(t_data *game, float new_x)
 {
-	float temp_x;
-	float temp_y;
+	float	temp_x;
+	float	temp_y;
 
-	if (game->map[(int)game->player.pos_y][(int)(new_x - 0.1)] != '1' 
+	if (game->map[(int)game->player.pos_y][(int)(new_x - 0.1)] != '1'
 		&& game->map[(int)game->player.pos_y][(int)(new_x + 0.1)] != '1'
-		&& game->map[(int)game->player.pos_y][(int)(new_x - 0.1)] != 'D' 
+		&& game->map[(int)game->player.pos_y][(int)(new_x - 0.1)] != 'D'
 		&& game->map[(int)game->player.pos_y][(int)(new_x + 0.1)] != 'D')
 	{
 		temp_x = new_x;
 		temp_y = game->player.pos_y;
 		if (game->map[(int)(temp_y - 0.1)][(int)(temp_x - 0.1)] != '1'
-            && game->map[(int)(temp_y - 0.1)][(int)(temp_x + 0.1)] != '1' 
-            && game->map[(int)(temp_y + 0.1)][(int)(temp_x - 0.1)] != '1' 
-            && game->map[(int)(temp_y + 0.1)][(int)(temp_x + 0.1)] != '1'
+			&& game->map[(int)(temp_y - 0.1)][(int)(temp_x + 0.1)] != '1'
+			&& game->map[(int)(temp_y + 0.1)][(int)(temp_x - 0.1)] != '1'
+			&& game->map[(int)(temp_y + 0.1)][(int)(temp_x + 0.1)] != '1'
 			&& game->map[(int)(temp_y - 0.1)][(int)(temp_x - 0.1)] != 'D'
-            && game->map[(int)(temp_y - 0.1)][(int)(temp_x + 0.1)] != 'D' 
-            && game->map[(int)(temp_y + 0.1)][(int)(temp_x - 0.1)] != 'D' 
-            && game->map[(int)(temp_y + 0.1)][(int)(temp_x + 0.1)] != 'D')
-        {
-            game->player.pos_x = new_x;
-        }
+			&& game->map[(int)(temp_y - 0.1)][(int)(temp_x + 0.1)] != 'D'
+			&& game->map[(int)(temp_y + 0.1)][(int)(temp_x - 0.1)] != 'D'
+			&& game->map[(int)(temp_y + 0.1)][(int)(temp_x + 0.1)] != 'D')
+			game->player.pos_x = new_x;
 	}
 }
 
-void try_y_movement(t_data *game, float new_y)
+void	try_y_movement(t_data *game, float new_y)
 {
-	float temp_x;
-	float temp_y;
+	float	temp_x;
+	float	temp_y;
 
-	if (game->map[(int)(new_y - 0.1)][(int)(game->player.pos_x)] != '1' 
-        && game->map[(int)(new_y + 0.1)][(int)(game->player.pos_x)] != '1'
-		&& game->map[(int)(new_y - 0.1)][(int)(game->player.pos_x)] != 'D' 
-        && game->map[(int)(new_y + 0.1)][(int)(game->player.pos_x)] != 'D')
-    {
+	if (game->map[(int)(new_y - 0.1)][(int)(game->player.pos_x)] != '1'
+		&& game->map[(int)(new_y + 0.1)][(int)(game->player.pos_x)] != '1'
+		&& game->map[(int)(new_y - 0.1)][(int)(game->player.pos_x)] != 'D'
+		&& game->map[(int)(new_y + 0.1)][(int)(game->player.pos_x)] != 'D')
+	{
 		temp_x = game->player.pos_x;
 		temp_y = new_y;
-        if (game->map[(int)(temp_y - 0.1)][(int)(temp_x - 0.1)] != '1'
-            && game->map[(int)(temp_y - 0.1)][(int)(temp_x + 0.1)] != '1' 
-            && game->map[(int)(temp_y + 0.1)][(int)(temp_x - 0.1)] != '1' 
-            && game->map[(int)(temp_y + 0.1)][(int)(temp_x + 0.1)] != '1'
+		if (game->map[(int)(temp_y - 0.1)][(int)(temp_x - 0.1)] != '1'
+			&& game->map[(int)(temp_y - 0.1)][(int)(temp_x + 0.1)] != '1'
+			&& game->map[(int)(temp_y + 0.1)][(int)(temp_x - 0.1)] != '1'
+			&& game->map[(int)(temp_y + 0.1)][(int)(temp_x + 0.1)] != '1'
 			&& game->map[(int)(temp_y - 0.1)][(int)(temp_x - 0.1)] != 'D'
-            && game->map[(int)(temp_y - 0.1)][(int)(temp_x + 0.1)] != 'D' 
-            && game->map[(int)(temp_y + 0.1)][(int)(temp_x - 0.1)] != 'D' 
-            && game->map[(int)(temp_y + 0.1)][(int)(temp_x + 0.1)] != 'D')
-        {
-            game->player.pos_y = new_y;
-        }
-    }
+			&& game->map[(int)(temp_y - 0.1)][(int)(temp_x + 0.1)] != 'D'
+			&& game->map[(int)(temp_y + 0.1)][(int)(temp_x - 0.1)] != 'D'
+			&& game->map[(int)(temp_y + 0.1)][(int)(temp_x + 0.1)] != 'D')
+		{
+			game->player.pos_y = new_y;
+		}
+	}
 }
 
 void	collision_check(t_data *game, float new_y, float new_x)
