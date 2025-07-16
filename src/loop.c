1f8eb56e2fbb1e7d1bd57327486205e8845ade60
Djax Bank <djax@djax.dev>
2025-07-16 16:24:23 +0200

    small change to the loop

--- src/loop.c
+++ src/loop.c
@@ -6,7 +6,7 @@
 /*   By: dbank <dbank@student.codam.nl>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 15:05:51 by showard           #+#    #+#             */
-/*   Updated: 2025/07/16 14:43:16 by dbank            ###   ########.fr       */
+/*   Updated: 2025/07/16 16:20:17 by dbank            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,29 +48,30 @@ static void    handle_key_d(t_data *game)
 		* (game->mlx.mlx->delta_time * 60.0);
 }
 
-void	loop_hook(void *param)
+void	loop_hook(void *game)
 {
-	t_data			*game;
-	const double	save[3] = {((t_data*)param)->player.pos_y,
-		((t_data *)param)->player.pos_x, ((t_data *)param)->player.orientation};
+	static double	save[3];
 
-	game = (t_data *)param;
 	handle_window_resize(game);
-	if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(game->mlx.mlx);
-	if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_ENTER))
+	if (mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(((t_data *)game)->mlx.mlx);
+	if (mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_ENTER))
 		open_door(game);
-	if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_W))
+	if (mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_W))
 		handle_key_w(game);
-	if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_S))
+	if (mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_S))
 		handle_key_s(game);
-	if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_A))
+	if (mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_A))
 		handle_key_a(game);
-	if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_D))
+	if (mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_D))
 		handle_key_d(game);
-	if (save[0] != game->player.pos_y || save[1] != game->player.pos_x
-		|| save[2] != game->player.orientation)
+	if (save[0] != ((t_data *)game)->player.pos_y
+		|| save[1] != ((t_data *)game)->player.pos_x
+		|| save[2] != ((t_data *)game)->player.orientation)
 	{
+		save[0] = ((t_data*)game)->player.pos_y;
+		save[1] = ((t_data *)game)->player.pos_x;
+		save[2] = ((t_data *)game)->player.orientation;
 		raycaster(game, false);
 		draw_minimap(game, false);
 	}
