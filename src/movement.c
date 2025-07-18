b5679557d81b7bc2ceec70ddd9fec8727d93c516
a0f <samspencerhoward@gmail.com>
2025-07-18 14:43:48 +0200

    arrow movmeent

--- src/movement.c
+++ src/movement.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   movement.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dbank <dbank@student.codam.nl>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/15 18:08:10 by showard           #+#    #+#             */
-/*   Updated: 2025/07/17 13:46:37 by dbank            ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   movement.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dbank <dbank@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/07/15 18:08:10 by showard       #+#    #+#                 */
+/*   Updated: 2025/07/18 13:22:38 by showard       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,28 +82,29 @@ static void	collision_check(t_data *game, float new_y, float new_x)
 
 static int	handle_key_movement(t_data *game, double *new_y, double *new_x)
 {
-	if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_W))
+	if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_W)
+		|| mlx_is_key_down(game->mlx.mlx, MLX_KEY_UP))
 		return (*new_y = game->player.pos_y + sin(game->player.orientation)
 			/ 20.0 * (game->mlx.mlx->delta_time * 60.0),
-			*new_x = game->player.pos_x + cos(game->player.orientation)
-			/ 20.0 * (game->mlx.mlx->delta_time * 60.0));
-	else if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_S))
+			*new_x = game->player.pos_x + cos(game->player.orientation) / 20.0
+			* (game->mlx.mlx->delta_time * 60.0));
+	else if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_S)
+		|| mlx_is_key_down(game->mlx.mlx, MLX_KEY_DOWN))
 		return (*new_y = game->player.pos_y - sin(game->player.orientation)
 			/ 20.0 * (game->mlx.mlx->delta_time * 60.0),
-			*new_x = game->player.pos_x - cos(game->player.orientation)
-			/ 20.0 * (game->mlx.mlx->delta_time * 60.0));
+			*new_x = game->player.pos_x - cos(game->player.orientation) / 20.0
+			* (game->mlx.mlx->delta_time * 60.0));
 	else if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_A))
 		return (*new_y = game->player.pos_y - cos(game->player.orientation)
 			/ 20.0 * (game->mlx.mlx->delta_time * 60.0),
-			*new_x = game->player.pos_x + sin(game->player.orientation)
-			/ 20.0 * (game->mlx.mlx->delta_time * 60.0));
+			*new_x = game->player.pos_x + sin(game->player.orientation) / 20.0
+			* (game->mlx.mlx->delta_time * 60.0));
 	else
 		return (*new_y = game->player.pos_y + cos(game->player.orientation)
 			/ 20.0 * (game->mlx.mlx->delta_time * 60.0),
-			*new_x = game->player.pos_x - sin(game->player.orientation)
-			/ 20.0 * (game->mlx.mlx->delta_time * 60.0));
+			*new_x = game->player.pos_x - sin(game->player.orientation) / 20.0
+			* (game->mlx.mlx->delta_time * 60.0));
 }
-
 void	keypress(t_data *game)
 {
 	double	new_y;
@@ -114,20 +115,18 @@ void	keypress(t_data *game)
 	if (mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_W)
 		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_S)
 		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_A)
-		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_D))
-		return (handle_key_movement(game, &new_y, &new_x),
-			collision_check(game, new_y, new_x));
+		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_D)
+		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_UP)
+		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_DOWN))
+		return (handle_key_movement(game, &new_y, &new_x), collision_check(game,
+				new_y, new_x));
 	else
 	{
 		if (mlx_is_key_down(game->mlx.mlx, MLX_KEY_LEFT))
-		{
-			game->player.orientation -= 0.05
-				*(game->mlx.mlx->delta_time * 60.0);
-		}
+			game->player.orientation -= 0.05 * (game->mlx.mlx->delta_time
+					* 60.0);
 		else
-		{
-			game->player.orientation += 0.05
-				* (game->mlx.mlx->delta_time * 60.0);
-		}
+			game->player.orientation += 0.05 * (game->mlx.mlx->delta_time
+					* 60.0);
 	}
 }
