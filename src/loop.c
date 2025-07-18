d859262601e689b8e4390869a71d07b2ea02b359
a0f <samspencerhoward@gmail.com>
2025-07-18 14:43:48 +0200

    Add arrow key movement

--- src/loop.c
+++ src/loop.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   loop.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dbank <dbank@student.codam.nl>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/15 15:05:51 by showard           #+#    #+#             */
-/*   Updated: 2025/07/17 13:46:49 by dbank            ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   loop.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dbank <dbank@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/07/15 15:05:51 by showard       #+#    #+#                 */
+/*   Updated: 2025/07/18 13:18:17 by showard       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,9 @@ void	loop_hook(void *game)
 		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_A)
 		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_D)
 		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_LEFT)
-		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_RIGHT))
+		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_RIGHT)
+		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_UP)
+		|| mlx_is_key_down(((t_data *)game)->mlx.mlx, MLX_KEY_DOWN))
 		keypress(game);
 	if (save[0] != ((t_data *)game)->player.pos_y
 		|| save[1] != ((t_data *)game)->player.pos_x
@@ -35,7 +37,6 @@ void	loop_hook(void *game)
 		save[0] = ((t_data *)game)->player.pos_y;
 		save[1] = ((t_data *)game)->player.pos_x;
 		save[2] = ((t_data *)game)->player.orientation;
-		raycaster(game, false);
-		draw_minimap(game, false);
+		(raycaster(game, false), draw_minimap(game, false));
 	}
 }
